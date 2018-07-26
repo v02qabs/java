68d802ae6f1cfa04d12ae8cb47a1e2e6cd9a354e
v02qabs <takesue090@gmail.com>
2018-07-26 23:06:25 +0900

    thello

--- AndroidJavaConsole/Main/Main.java
+++ AndroidJavaConsole/Main/Main.java
@@ -1,8 +1,11 @@
+
 class Main
 {
 	public static void main(String[] args)
 	{
 		System.out.println("Hello.");
+		item i = new item();
+		i.show("Hello.S");
 	}
 }
 
---
+++ AndroidJavaConsole/Main/item.java
@@ -0,0 +1,8 @@
+class item
+{
+	public void show(String message)
+	{
+		System.out.println(message);
+	}
+}
+
