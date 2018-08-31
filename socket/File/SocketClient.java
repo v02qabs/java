a93dc63ae04a66d0f9aedf9804dbef224c0e8856
v02qabs <takesue090@gmail.com>
2018-08-31 10:28:06 +0900

    remove

--- socket/File/SocketClient.java
+++ socket/File/SocketClient.java
@@ -6,8 +6,8 @@ import java.io.OutputStream;
 import java.net.Socket;
 
 public class SocketClient {
-	final static String HOST = "192.168.0.6"; // 接続先アドレス
-	final static int    PORT = 8001;        // 接続先ポート番号
+	final static String HOST = "ftp.chobi.net"; // 接続先アドレス
+	final static int    PORT = 8080;        // 接続先ポート番号
 
 	public static void main(String[] args) {
 		String filepath = "f.in";             // 送信するファイルのパス
