264daa045680ea7d1e7fe68c6194d099fec2387b
v02qabs <takesue090@gmail.com>
2018-11-08 20:35:30 +0900

    Hello

--- HelloVer1-Java/HelloVer1/src/hellover1/HelloVer1.java
+++ HelloVer1-Java/HelloVer1/src/hellover1/HelloVer1.java
@@ -16,7 +16,7 @@ public class HelloVer1 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Hello1.");
+        System.out.println("Hello1");
     }
     
 }
