81bf096c7551dcae1068c26adc7f209080671021
v02qabs <takesue090@gmail.com>
2019-07-17 12:58:12 +0900

    Mr.Ikuta

---
+++ mr.ikuta/Main.java
@@ -0,0 +1,15 @@
+public class Main
+{
+	public Main(){}
+	public static void main(String[] args)
+	{
+		Main m = new Main();
+		m.init();
+	}
+	public void init()
+	{
+		System.out.println("init");
+		
+	}
+}
+
---
+++ mr.ikuta/install.sh
@@ -0,0 +1,2 @@
+ecj Main.java
+dx --dex --output=Main.jar Main.class
