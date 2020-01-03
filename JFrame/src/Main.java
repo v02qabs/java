fcc0943d6bf55bed1dde4fdb5beb0f6294028d64
v02qabs <takesue090@gmail.com>
2020-01-03 14:38:47 +0900

    JFrame-Main-Init-File

---
+++ JFrame/src/Main.java
@@ -0,0 +1,15 @@
+import javax.swing.JFrame;
+
+public class Main extends JFrame{
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+			new Main();
+	}
+	public Main()
+	{
+		setBounds(0,0,500,500);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setVisible(true);
+	}
+}
