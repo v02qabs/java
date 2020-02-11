ae2e304c746c99b381f1d503a239b22236a9afee
v02qabs <takesue090@gmail.com>
2020-02-12 07:54:02 +0900

    Java-awt-Buton-Change-Default

--- awt/Main.java
+++ awt/Main.java
@@ -1,24 +1,19 @@
 import java.awt.*;
 
+
 public class Main extends Frame
 {
-	public static void main(String[] args)
-	{
-		new Main().Init();
-	}
-	public void Init()
-	{
-		setBounds(0,0,500,500);
-		setTitle("Hello");
-		setParts();
-		setVisible(true);
-	}
-	public void setParts()
-	{
-		Button b = new Button("OK");
-		setLayout(null);
-		b.setBounds(0,0,100,30);
-		add(b);
-	}
-}
 
+
+    public static void main(String [] args) {
+        new Main();
+    }
+    Main() {
+        super("ButtonTest");
+        setSize(200, 100);
+        setLayout(new FlowLayout());
+        Button b1 = new Button("OK");
+        add(b1);
+        show();
+    }
+}
