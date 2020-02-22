04d1f59aa45912fc4318f52dba422ae76efd235e
v02qabs <takesue090@gmail.com>
2020-02-23 02:51:59 +0900

    gdx-key-move-image-x++

--- gdx/core/src/hello/gdx/MyHelloNo1.java
+++ gdx/core/src/hello/gdx/MyHelloNo1.java
@@ -15,46 +15,33 @@ public class MyHelloNo1 extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
 	Music s;
+	private Stage stage;
+	private Table t;
+	private Image i;
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
 		s = Gdx.audio.newMusic(Gdx.files.internal("bomb2.mp3"));
 	}
-
+	int x = 0;
 	@Override
 	public void render () {
 		Gdx.gl.glClearColor(1, 1, 1, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		//batch.begin();
-		//batch.draw(img, 0, 0);
-		//batch.end();
-		/*
+		//x = x + 1;
 		if(Gdx.input.isKeyPressed(Input.Keys.LEFT))
 		{
-			s.play();
-			
+			//s.play();
+			x++;
 		}
-		batch.end();*/
-		int si = 0;
-		setup(si);
-		if(Gdx.input.isKeyPressed(Input.Keys.ALT_LEFT))
-		{
-			setup(100);
-		}
+		batch.begin();
+		//batch.draw(img, 0, 0);
+		//batch.end();
+		//batch.end();
+		batch.draw(img, x, 10, 100,100);
+		batch.end();
 	}
-	
-	private void setup(int si) {
-		// TODO Auto-generated method stub
-		Stage stage = new Stage();
-		Table t = new Table();
-		t.setBounds(si, 0, 100, 100);
-		Image i = new Image(img);
-		t.add(i);
-		stage.addActor(t);
-		stage.draw();
-	}
-
 	@Override
 	public void dispose () {
 		batch.dispose();
