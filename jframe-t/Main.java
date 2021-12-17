9176680b98f369ef452c89ee1023dc184a8325cd
v02qabs <takesue090@gmail.com>
2021-12-17 13:16:17 +0900

    JFrame-x64

---
+++ jframe-t/Main.java
@@ -0,0 +1,18 @@
+import javax.swing.*;
+import java.io.*;
+
+public class Main extends JFrame
+{
+	public static void main(String[] args)
+		{
+			new Main().init();
+			}
+
+			private void init()
+			{
+				setSize(600,600);
+				setVisible(true);
+			}
+	}
+
+
