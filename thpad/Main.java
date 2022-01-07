1fc7148b88733ae64bddad56eefb972b55de4ad7
v02qabs <takesue090@gmail.com>
2022-01-07 22:34:07 +0900

    gui-swing-made-pdf-doc

---
+++ thpad/Main.java
@@ -0,0 +1,18 @@
+import java.awt.*;
+import javax.swing.*;
+import java.awt.event.*;
+
+
+class Main{
+	public Main()
+	{
+	}
+	public static void main(String[] args)
+	{
+		new thpad();
+	}
+}
+
+class thpad
+			{
+				
