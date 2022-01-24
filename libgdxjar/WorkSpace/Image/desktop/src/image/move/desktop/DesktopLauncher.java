5793521eda34fb2534ea6b61ae9ff351d0ada60a
v02qabs <takesue090@gmail.com>
2022-01-24 20:06:09 +0900

    han

---
+++ libgdxjar/WorkSpace/Image/desktop/src/image/move/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package image.move.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import image.move.MyImageMove;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MyImageMove(), config);
+	}
+}
