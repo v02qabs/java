237c3390d44e3060adc1b22fa28d5c4a24c91e93
v02qabs <takesue090@gmail.com>
2023-08-15 12:36:51 +0900

    add-readData

---
+++ quize/MainActivity.java
@@ -0,0 +1,14 @@
+import java.io.";
+
+public class MainActivity
+{
+	public MainActivity&()
+	{
+		new readData();
+	}
+	public static void main(String[] args)
+	{
+		new MainActivity();
+	}
+}
+
---
+++ quize/read-data.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.nio.file.*;
+
+class readData
+{
+	public readData()
+	{
+		System.out.println("Hello read data class");
+		readData();
+	}
+	private void readData()
+	{
+		Path data_path = Paths.get("./korian.txt");
+		try
+		{
+			String korian_data = Files.readString(data_path);
+			System.out.println(korian_data);
+		}
+		catch(Exception error)
+		{
+			System.out.println(error.toString());
+		}
+	}
+}
+
