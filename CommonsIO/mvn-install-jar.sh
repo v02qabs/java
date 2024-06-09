084b2181e2e98ef7fc376e0e022116daeff30688
v02qabs <takesue090@gmail.com>
2024-06-09 20:35:13 +0900

    commonsio-last-20:36

---
+++ CommonsIO/mvn-install-jar.sh
@@ -0,0 +1,2 @@
+mvn install:install-file -Dfile=$1 -DgroupId=$2 -DartifactId=$3 -Dversion=1.0 -Dpackaging=jar
+
--- CommonsIO/my-app/src/main/java/hiro/com/Downloads/App.java
+++ CommonsIO/my-app/src/main/java/hiro/com/Downloads/App.java
@@ -1,5 +1,10 @@
 package hiro.com.Downloads;
 
+import org.apache.commons.io.FileUtils;
+import java.io.File;
+import java.net.URL;
+
+
 /**
  * Hello world!
  *
@@ -9,5 +14,23 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        new App().use_Downloads_commons_IO();
     }
+    private void use_Downloads_commons_IO(){
+       try{
+        downloadDatabase("https://hirotakeakesi.work", "/home/takesue090/logging.txt x +			");
+        System.out.println("success");
+        }
+        catch(Exception error){
+        	System.out.println(error.toString());
+        	}
+        	
+    }
+
+    public static void downloadDatabase(String urlString, String localFilePath) throws Exception {
+        URL url = new URL(urlString);
+        File file = new File(localFilePath);
+        FileUtils.copyURLToFile(url, file);
+    }
+
 }
